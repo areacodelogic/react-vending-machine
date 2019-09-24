04b76a6cbc37398b907789613759b0041a66cbbb
Kimberly Johnson <kajohnson3140@gmail.com>
2019-09-24 12:50:57 -0700

    added items to state, message if out of stock

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Candy from "./Candy";
 import Chips from "./Chips";
 import Soda from "./Soda";
@@ -9,17 +9,86 @@ import "./App.css";
 import VendingMachine from "./VendingMachine";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
 
-  render(){
+    this.state = {
+      chipsInStock: {
+        fritos: {
+          inStock: true,
+          amtLeft: 10
+        },
+        cheetos: {
+          inStock: true,
+          amtLeft: 10
+        },
+        lays: {
+          inStock: true,
+          amtLeft: 10
+        },
+        ruffles: {
+          inStock: true,
+          amtLeft: 10
+        }
+      },
+      candyInStock: {
+        mnm: {
+          inStock: true,
+          amtLeft: 10
+        },
+        snickers: {
+          inStock: true,
+          amtLeft: 10
+        },
+        sweedishFish: {
+          inStock: true,
+          amtLeft: 10
+        },
+        rolos: {
+          inStock: true,
+          amtLeft: 10
+        },
+        starbursts: {
+          inStock: true,
+          amtLeft: 10
+        }
+      },
+      sodaInStock: {
+        cocaCola: {
+          inStock: false,
+          amtLeft: 10
+        },
+        orangeCrush: {
+          inStock: false,
+          amtLeft: 10
+        },
+        rootBeer: {
+          inStock: false,
+          amtLeft: 10
+        }
+      }
+    }
+  }
+
+  render() {
+    let chips = this.state.chipsInStock;
+    let candy = this.state.candyInStock;
+    let soda = this.state.sodaInStock;
 
     return (
       <div className="App">
         <BrowserRouter>
           <Navbar />
-          <Route exact path="/" render={() => <VendingMachine />} />
-          <Route exact path="/candy" render={() => <Candy />} />
-          <Route exact path="/soda" render={() => <Soda />} />
-          <Route exact path="/chips" render={() => <Chips />} />
+          <Route exact path="/" render={() =>
+            <VendingMachine
+              chips={chips}
+              candy={candy}
+              soda={soda}
+            />}
+          />
+          <Route exact path="/chips" render={() => <Chips chips={chips} />} />
+          <Route exact path="/candy" render={() => <Candy candy={candy} />} />
+          <Route exact path="/soda" render={() => <Soda soda={soda} />} />
         </BrowserRouter>
       </div>
     );
--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,16 +1,52 @@
 import React, { Component } from "react";
 import VendingPic from "./download.jpeg"
-import {Link} from "react-router-dom"
-
+import { Link } from "react-router-dom"
 
 class VendingMachine extends Component {
   render() {
+    let fritos = this.props.chips.fritos.inStock;
+    let cheetos = this.props.chips.fritos.inStock;
+    let lays = this.props.chips.fritos.inStock;
+    let ruffles = this.props.chips.fritos.inStock;
+    let chips = [fritos, cheetos, lays, ruffles];
+
+    let mnm = this.props.candy.mnm.inStock;
+    let snickers = this.props.candy.snickers.inStock;
+    let sweedishFish = this.props.candy.sweedishFish.inStock;
+    let rolos = this.props.candy.rolos.inStock;
+    let starbursts = this.props.candy.starbursts.inStock;
+    let candy = [mnm, snickers, sweedishFish, rolos, starbursts];
+
+    let cocaCola = this.props.soda.cocaCola.inStock;
+    let orangeCrush = this.props.soda.orangeCrush.inStock;
+    let rootBeer = this.props.soda.rootBeer.inStock;
+    let soda = [cocaCola, orangeCrush, rootBeer];
+
+    let chipsOut;
+    let candyOut;
+    let sodaOut;
+    
+    if (chips.every(chip => chip === false)) {
+      chipsOut = <p>Chips are out of stock</p>;
+    }
+
+    if (candy.every(candy => candy === false)) {
+      candyOut = <p>Candy is out of stock</p>;
+    }
+
+    if (soda.every(soda => soda === false)) {
+      sodaOut = <p>Soda is out of stock</p>;
+    }
+
     return (
       <div className="VendingMachine">
-      <img src={VendingPic} alt="snack machine"/>
-      <p>Go to <Link to="/candy">M&Ms</Link> page</p>
-      <p>Go to <Link to="/soda">Soda</Link> page</p>
-      <p>Go to <Link to="/chips">Chips</Link> page</p>
+        <img src={VendingPic} alt="snack machine" />
+        <p>Go to <Link to="/chips">Chips</Link> page</p>
+        {chipsOut}
+        <p>Go to <Link to="/candy">Candy</Link> page</p>
+        {candyOut}
+        <p>Go to <Link to="/soda">Soda</Link> page</p>
+        {sodaOut}
       </div>
     );
   }
